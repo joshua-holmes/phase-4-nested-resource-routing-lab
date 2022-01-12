5e6e9ffd8d2226c530c6e08422b0288b7ba3f706
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-01-12 00:31:47 -0600

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::API
+    rescue_from ActiveRecord::RecordNotFound, with: :render_not_found
+
+    private
+
+    def render_not_found e
+        render json: { errors: e.message }, status: 404
+    end
 end
