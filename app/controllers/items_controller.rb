8c7659910fd926e41f008ad6ece595331d5b884a
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-01-12 00:31:39 -0600

    commit tree 6ec76e423fb24c0c202e8fcdc7eff1ac8fe05803

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,26 @@
 class ItemsController < ApplicationController
 
   def index
-    items = Item.all
-    render json: items, include: :user
+    if params[:user_id]
+      user = User.find(params[:user_id])
+      render json: user.items, include: :user
+    else
+      render json: Item.all, include: :user
+    end
+  end
+  def show
+    item = Item.find(params[:id])
+    render json: item
+  end
+  def create
+    item = Item.create!(item_params)
+    render json: item, status: 201
+  end
+
+  private
+
+  def item_params
+    params.permit(:price, :description, :name, :user_id)
   end
 
 end
